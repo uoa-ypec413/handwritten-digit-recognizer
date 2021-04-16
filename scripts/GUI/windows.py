2853e9163b6538344ad503a2813ae797feda1305
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-04-16 19:08:06 +1200

    Add menu bar
    Add menu bar to main window with options "File" and "View"

--- scripts/GUI/windows.py
+++ scripts/GUI/windows.py
@@ -1,10 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QDesktopWidget, QAction
 
-class Window(QWidget):
-
+class MainWindow(QMainWindow):
+    
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Handwritten Digit Recognizer")
+        self.resize(1000, 800)
+        self.center()
+        self.addMenuBar()
+        self.show()
 
     def center(self):
         qRectangle = self.frameGeometry() # get window geometry
@@ -12,16 +17,12 @@ class Window(QWidget):
         qRectangle.moveCenter(centerPosition)
         self.move(qRectangle.topLeft()) # move window to monitor centre position
 
-class MainWindow(Window):
-    
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Handwritten Digit Recognizer")
-        self.resize(1000, 800)
-        self.center()
-        self.show()
+    def addMenuBar(self):
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        viewMenu = menubar.addMenu('&View')
 
-class TrainingWindow(Window):
+class TrainingWindow(QWidget):
 
     def __init__(self):
         super().__init__()
@@ -29,6 +30,12 @@ class TrainingWindow(Window):
         self.resize(400, 400)
         self.center()
         self.show()
+
+    def center(self):
+        qRectangle = self.frameGeometry() # get window geometry
+        centerPosition = QDesktopWidget().availableGeometry().center() # get monitor center position
+        qRectangle.moveCenter(centerPosition)
+        self.move(qRectangle.topLeft()) # move window to monitor centre position
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
