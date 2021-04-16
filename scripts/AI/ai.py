22e3c08fb43b589cf60520a5ad0788759778d8e4
Keith Anderson <kand198@aucklanduni.ac.nz>
2021-04-16 16:58:34 +1200

    Import MNIST dataset and load into data objects

--- scripts/AI/ai.py
+++ scripts/AI/ai.py
@@ -9,4 +9,25 @@ from torchvision import datasets, transforms
 import torch.nn.functional as F
 import time # Lets us timestamp things nicely, makes working with time units simpler
 
+# Training settings
+batch_size = 32
+device = 'cuda' if cuda.is_available() else 'cpu'
 
+# Import the MNIST Dataset from Torchvision.
+train_dataset = datasets.MNIST(root='mnist_data/',
+                               train=True,
+                               transform=transforms.ToTensor(),
+                               download=True)
+
+test_dataset = datasets.MNIST(root='mnist_data/',
+                              train=False,
+                              transform=transforms.ToTensor())
+
+# Data Loader (Input Pipeline)
+train_loader = data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
