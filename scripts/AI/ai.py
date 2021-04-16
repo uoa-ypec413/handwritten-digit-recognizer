ebb5ac95aae14a8a17bfcde7eb91ff9f3f58d816
Keith Anderson <kand198@aucklanduni.ac.nz>
2021-04-16 17:17:38 +1200

    Set up a basic neural network and training

--- scripts/AI/ai.py
+++ scripts/AI/ai.py
@@ -31,3 +31,38 @@ train_loader = data.DataLoader(dataset=train_dataset,
 test_loader = data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        # Start with a basic 2-layer neural network
+        self.l1 = nn.Linear(784, 392)
+        self.l2 = nn.Linear(392, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 784)  # Flatten the data (n, 1, 28, 28)-> (n, 784)
+        x = F.relu(self.l1(x))
+        return self.l2(x)
+
+# Create the model
+model = Net()
+model.to(device)
+# Use cross entropy loss function
+criterion = nn.CrossEntropyLoss()
+# Try optimising with basic stochastic gradient descent setup first
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9
+
+def train(epoch):
+    model.train() # Puts the model into training mode
+    for batch_idx, (data, target) in enumerate(train_loader): # iterate through the training dataset
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} | Batch Status: {}/{} ({:.0f}%) | Loss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item())))
