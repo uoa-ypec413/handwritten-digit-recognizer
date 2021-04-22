d89aa65fe95cb9888f9c39f9955ba521e56895b7
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-04-22 20:00:47 +1200

    Add adjusted LeNet-5

--- scripts/AI/NN.py
+++ scripts/AI/NN.py
@@ -15,7 +15,7 @@ class StandardNN(nn.Module):
         return self.l2(x)
 
 class LeNet5(nn.Module):
-
+    
     def __init__(self):
         super(LeNet5, self).__init__()
         # Image is padded to get an input size of 32x32
@@ -33,5 +33,27 @@ class LeNet5(nn.Module):
         x = x.view(-1, 16*5*5)
         x = F.tanh(self.fc1(x))
         x = F.tanh(self.fc2(x))
+        x = F.log_softmax(self.fc3(x))
+        return x
+
+class AdjustedLeNet5(nn.Module):
+    
+    def __init__(self):
+        super(AdjustedLeNet5, self).__init__()
+        # Image is padded to get an input size of 32x32
+        self.conv1 = nn.Conv2d(in_channels= 1, out_channels= 6, kernel_size= 5, stride= 1, padding= 2)
+        self.conv2 = nn.Conv2d(in_channels= 6, out_channels= 16, kernel_size= 5, stride= 1)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(input= x, kernel_size= 2, stride= 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(input= x, kernel_size= 2, stride= 2)
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = F.log_softmax(self.fc3(x))
         return x
