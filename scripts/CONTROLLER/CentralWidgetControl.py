69ce739e9956f0dc68b1eea4e226f4de8272c448
uoa-ypec413 <ypec413@aucklanduni.ac.nz>
2021-04-27 10:43:30 +1200

    Combine controllers for simplicity

--- scripts/CONTROLLER/CentralWidgetControl.py
+++ scripts/CONTROLLER/CentralWidgetControl.py
@@ -1,10 +1,43 @@
-from CONTROLLER.CanvasControl import *
-from CONTROLLER.ProbabilityPlotControl import *
 from GUI.centralwidget import *
+from GUI.canvas import *
+from GUI.probabilityplot import *
+
+class ProbabilityPlotControl():
+    def __init__(self):
+        self.plot = probabilityPlot()
+    
+    def setProbability(self, probabilities):
+        self.plot.setProbability(probabilities)
+
+class CanvasController():
+    def __init__(self):
+
+        self.canvas = Canvas(self)
+        self.lastPos = None
+
+    def onMouseMove(self, position):
+        if self.lastPos is None: # On the first mouse event, do not paint, just save position
+            self.lastPos = position
+        else:
+            currentPos = position
+            self.canvas.drawLine(self.lastPos, currentPos)
+            self.lastPos = currentPos
+    
+    def onMouseRelease(self):
+        self.lastPos = None
+        self.saveDrawing()
+    
+    def clear(self):
+        self.canvas.clear()
+
+    def saveDrawing(self):
+
+            drawing = self.canvas.pixmap()
+            drawing.save('user_data/digit_drawing.jpg', "JPEG" )
 
 class CentralWidgetControl():
     def __init__(self, mainWindow, digit_recogniser_controller):
-        self.canvasController = canvasController()
+        self.canvasController = CanvasController()
         self.probabilityController = ProbabilityPlotControl()
         self.digit_recogniser_controller = digit_recogniser_controller
 
--- scripts/CONTROLLER/ViewerWindowController.py
+++ scripts/CONTROLLER/ViewerWindowController.py
@@ -1,5 +1,19 @@
 from GUI.viewerwindow import *
-from CONTROLLER.ImageFrameController import *
+from GUI.imageframe import *
+
+class ImageFrameController():
+    def __init__(self):
+        self.imageFrame = ImageFrame()
+    
+    def updatePage(self, page):
+        self.imageFrame.setPage(page)
+        self.load_dataset(self.dataset)
+    
+    def load_dataset(self, dataset):
+        self.dataset = dataset
+        #self.imageFrame.clearGrid()
+        self.imageFrame.render_dataset(dataset)
+        #self.imageFrame.addImages()
 
 class ViewerWindowController():
     def __init__(self, digit_recogniser_controller):
